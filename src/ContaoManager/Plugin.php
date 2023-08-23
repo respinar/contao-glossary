be9fea98f34b34c8502bbd77fb94274885d4d47e
Hamid Peywasti <hamid.peywasti@gmail.com>
2023-08-23 12:55:17 +0330

    remove routings

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -15,30 +15,16 @@ namespace Respinar\GlossaryBundle\ContaoManager;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
-use Symfony\Component\Config\Loader\LoaderResolverInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Routing\RouteCollection;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Respinar\GlossaryBundle\RespinarGlossaryBundle;
 
-class Plugin implements BundlePluginInterface, RoutingPluginInterface
+class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create('Respinar\GlossaryBundle\RespinarGlossaryBundle')
-                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+            BundleConfig::create(RespinarGlossaryBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
         ];
-    }
-
-    /**
-     * @throws \Exception
-     *
-     * @return RouteCollection|null
-     */
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
-    {
-        return $resolver
-            ->resolve(__DIR__.'/../../config/routes.yaml')
-            ->load(__DIR__.'/../../config/routes.yaml');
     }
 }
