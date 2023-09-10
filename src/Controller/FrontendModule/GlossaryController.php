565bf6c82884c93ca4cd71a5d88ff7c090f20471
Hamid Peywasti <hamid@respinar.com>
2023-09-10 23:29:00 +0330

    change to termDetail

--- src/Controller/FrontendModule/GlossaryController.php
+++ src/Controller/FrontendModule/GlossaryController.php
@@ -70,7 +70,7 @@ class GlossaryController extends AbstractFrontendModuleController
 		$objTemplate->setData($objTerm->row());
 
     $objTemplate->link = $objTerm->url;
-    $objTemplate->moreDetail = $GLOBALS['TL_LANG']['MSC']['moreDetail']; // more Detail
+    $objTemplate->moreDetail = $GLOBALS['TL_LANG']['MSC']['termDetail']; // more Detail
 
     if ($objTerm->imgSRC)
         {
